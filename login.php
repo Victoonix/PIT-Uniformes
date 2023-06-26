20303d8c3ddb45c0a254f981a07b0eb48757f2bd
Victor Canela <99288626+Victoonix@users.noreply.github.com>
2023-06-26 12:58:15 -0300

    Muitas mudanças no funcionamento do php
    Nova pagina única de cadastro para todos os tipos, e código de php reescrito inteiramente

---
+++ login.php
@@ -0,0 +1,65 @@
+<?php
+    session_start();
+    header("Content-Type: text/html; charset=utf8");
+
+    $tipo = !isset($_GET["tipo"]) ? "listar" : $_GET["tipo"];
+
+    $con = new mysqli("localhost", "root", "", "uniformes");
+    switch ($tipo)
+    {
+        //escola
+        case 'escola':
+            $email = $_POST['email']; $senha = $_POST['senha'];
+
+            $stmt = $con->prepare("select count(id_escola) from escola where email = ? and senha = ?");
+                    $stmt->bind_param("ss", $email, $senha);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+            if ($result->$num_rows >= 1)
+            {
+                echo 'Login concluído';
+            }
+            else
+            {
+                echo 'Email ou senha incorretos';
+            }
+        break;
+        
+        //fornecedor
+        case 'fornecedor':
+            $email = $_POST['email']; $senha = $_POST['senha'];
+
+            $stmt = $con->prepare("select count(id_fornecedor) from fornecedor where email = ? and senha = ?");
+                    $stmt->bind_param("ss", $email, $senha);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+            if ($result->$num_rows >= 1)
+            {
+                echo 'Login concluído';
+            }
+            else
+            {
+                echo 'Email ou senha incorretos';
+            }
+        break;
+
+        //aluno
+        case 'aluno':
+        $token = $_POST['token'];
+
+        $stmt = $con->prepare("select count(token) from escola where token = ?");
+                    $stmt->bind_param("i", $token);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+            if ($result->$num_rows >= 1)
+            {
+                echo 'Login concluído';
+            }
+            else
+            {
+                echo 'Token incorreto. Caso seja um engano, verifique com a sua instituição';
+            }
+        break;
+    }
+    $con->close();
+?>
--- uniformes.sql
+++ uniformes.sql
@@ -5,23 +5,14 @@ create table escola (
 id_escola int primary key not null auto_increment,
 nome varchar(80) not null,
 email varchar(80) not null,
-senha varchar(80) not null
-);
-
-create table aluno (
-matricula int primary key not null,
 senha varchar(80) not null,
-id_escola int not null,
-foreign key (id_escola) references escola(id_escola)
-);
+token int not null
+) engine=innodb;
 
 create table fornecedor (
 id_fornecedor int not null primary key auto_increment,
-nome varchar(80) not null,
 email varchar(80) not null,
-senha varchar(80) not null,
-id_escola int not null,
-foreign key (id_escola) references escola(id_escola)
-);
-select * from aluno;
+senha varchar(80) not null
+) engine=innodb;
+
 select * from escola;
