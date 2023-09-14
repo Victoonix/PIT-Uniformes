11420835a1cf937ddd42d1ce02f9ab876f688cdd
Victor Canela <99288626+Victoonix@users.noreply.github.com>
2023-09-14 08:07:57 -0300

    Juntei as páginas do João Tadeu

---
+++ PIT-Uniformes-Sprint-3/PIT-Uniformes/vendas.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+
+  <head>
+    <meta charset="utf-8" />
+    <title>RVK STORE - Home</title>
+    <link rel="stylesheet" type="text/css" href="css\styleVendas.css" />
+    <?php
+      header('Content-Type: text/html; charset=utf-8');
+    ?>
+  </head>
+
+  <body>
+
+    <div class="landing-page">
+      <header>
+        <div class="container">
+          <a href="#" class="logo">RVK <b>Uniformes</b></a>
+            <?php
+            session_start();
+            if($_SESSION['admin'] === true) {
+            echo '<a href="pagsCadastro/pagCadUniforme.html"><button class="botao-cadastro">Cadastrar uniforme</button></a>';
+            }
+            ?>   
+        </div>
+      </header>
+    </div>
+
+    <div class="GradeS">
+    <h1>CATÁLOGO COTEMIG</h1>
+    </div>
+
+    <br><br>
+
+    <div class="GradeI">
+      <h2>Produtos Masculinos</h2>
+    </div>
+
+  <div class="catalogo">
+    <div class="produto">
+      <a href="">
+        <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+        <h3>Camisa Cotemig</h3>
+        <p>---------------------------</p>
+        <p class="preco">R$ 00,00</p>
+      </a>
+    </div>
+
+  <div class="produto">
+    <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+    </a>
+    </div>
+
+  <div class="produto">
+    <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+    </a>
+    </div>
+
+  <div class="produto">
+    <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+    </a>
+    </div>
+
+  <div class="produto">
+    <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+    </a>
+  </div>
+</div>
+
+  <div class="GradeI">
+    <h2>Produtos Femininos</h2>
+  </div>
+
+  <div class="catalogo">
+    <div class="produto">
+      <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+    </a>
+    </div>
+
+    <div class="produto">
+      <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+      </a>
+    </div>
+
+    <div class="produto">
+      <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+      </a>
+    </div>
+
+    <div class="produto">
+      <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+      </a>
+    </div>
+
+    <div class="produto">
+      <a href="">
+      <img src="https://imgcentauro-a.akamaihd.net/700x700/96058602/camiseta-puma-manga-curta-essentials-logo-tee-masculina-img.jpg" alt="Camiseta Puma Manga Curta Essentials Logo Tee - Masculina" style="max-width: 200px;">
+      <h3>Camisa Cotemig</h3>
+      <p>---------------------------</p>
+      <p class="preco">R$ 00,00</p>
+      </a>
+    </div>
+  </div>
+
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <h6>SOBRE</h6>
+          <p class="text-justify">Bem-vindo à RVK STORE, onde a excelência encontra a uniformidade! Somos uma empresa de uniformes iniciantes dedicada a fornecer soluções de vestuário profissional que combinem estilo, qualidade e funcionalidade. Com um compromisso com a inovação e o atendimento excepcional, estamos determinados a nos tornar uma referência no setor de uniformes. Na RVK STORE, entendemos a importância dos uniformes como uma ferramenta para a identidade visual e a representação de uma marca. Sabemos que um uniforme bem projetado e de alta qualidade pode não apenas unificar a equipe, mas também transmitir profissionalismo e confiança aos clientes. É por isso que nos esforçamos para criar uniformes que atendam às necessidades específicas de cada setor, mantendo a elegância e o conforto.</p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+       <a href="#">Scanfcode</a>.
+          </p>
+    </div>
+
+    <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i>F</a></li>
+            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i>T</a></li>
+            <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i>I</a></li>
+            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i>L</a></li>   
+          </ul>
+        </div>
+      </div>
+    </div>
+</footer>
+
+  </body>
+</html>
